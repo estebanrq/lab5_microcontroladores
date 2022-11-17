3e1f10a5459325a7e365ac63f57f9730a3418f23
Esteban Rodriguez Quintana <erquintana0805@gmail.com>
2022-11-16 19:17:17 -0600

    updating csv reader

--- csv_writer.py
+++ csv_writer.py
@@ -1,32 +1,27 @@
 import serial
 import csv
-
+import time
+OUTPUT_MOVEMENT = "flex"
+THRESHOLD = 10
 PuertoSerial = serial.Serial(port = '/dev/ttyACM0', baudrate=115200, timeout=1) 
+
 print("Connected")
 data_rows = []
 header = ['x', 'y', 'z']
 # abrir o crear archivo en modo escritura
-f = open('leftright.csv', 'w', encoding='UTF8')  # nombre de archivo = gesto a grabar
+f = open(OUTPUT_MOVEMENT+'.csv', 'w', encoding='UTF8')  # nombre de archivo = gesto a grabar
 # create the csv writer
 writer = csv.writer(f)
 print(header)
 writer.writerow(header)  # escribe encabezado
-
-SAMPLES = 2000
-counter = 0
-
-while(counter < SAMPLES):
-#while():
-    
+ 
+ti = time.perf_counter()
+print("\t> Data gathering began")
+while(tf-ti < THRESHOLD): 
     input = PuertoSerial.readline().decode('utf-8').replace('\r', "").replace('\n', "")
     input = input.split('\t')
-    #print(input[2])
-    if len(input) == 3:   # avoids missing axes on first reading
-        writer.writerow(input)
-        print(input)
-    counter+=1
-
-
-
-    
-
+    #if len(input) == 3:   # avoids missing axes on first reading
+    #    writer.writerow(input)
+    #    print(input)
+    tf = time.perf_counter()
+print("\t> Data is available")
